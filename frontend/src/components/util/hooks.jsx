afc7a8928275f4d151617bf162169278745a2710
selk07 <70597627+selk07@users.noreply.github.com>
2024-11-28 20:15:42 +0200

    Update hooks.jsx

--- frontend/src/components/util/hooks.jsx
+++ frontend/src/components/util/hooks.jsx
@@ -1,24 +1,25 @@
 import axios from 'axios';
 import { URL } from './constants'
 
-export const getProducts = async () => {
+const getProducts = async () => {
   const response = await axios.get(`${URL}/products`);
   return response.data;
 };
 
-export const addProduct = async (product) => {
+const addProduct = async (product) => {
   const response = await axios.post(`${URL}/products`, product);
   return response.data;
 };
 
-export const deleteProduct = async (id) => {
+const deleteProduct = async (id) => {
   await axios.delete(`${URL}/products/${id}`);
 };
 
-export const updateProduct = async (id, product) => {
+const updateProduct = async (id, product) => {
    const response = await axios.put(`${URL}/products/${id}`, product);
    return response.data;
  };
+export {getProducts, addProduct, deleteProduct, updateProduct}
  
 
 
